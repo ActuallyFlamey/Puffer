a82b96605c2377edff922047d59ed6f56fdd31f9
Flamey <flamey@hexa.blue>
2024-04-09 17:10:34 +0200

    woah

--- bot.py
+++ bot.py
@@ -1,17 +1,21 @@
 import discord
 import json
+import ast
 from discord import app_commands as app
 
 with open("./token.json") as tokenfile:
     token = json.load(tokenfile)["puffer"]
 
-bot = discord.Client(intents=discord.Intents.default())
+bot = discord.Client(intents=discord.Intents.all())
 tree = app.CommandTree(bot)
 
 guild = discord.Object(id=1226549383781290205)
 
 def is_admin(ctx: discord.Interaction):
     return ctx.user.guild_permissions.administrator
+
+def is_dev(ctx: discord.Interaction):
+    return ctx.user.id == 450678229192278036
 
 @bot.event
 async def on_ready():
@@ -21,12 +25,11 @@ async def on_ready():
 async def ping(ctx: discord.Interaction):
     await ctx.response.send_message("pong :3")
 
-@tree.command(name="mkembed", description="Creates a message with an embed.", guild=guild)
-@app.check(is_admin)
-async def mkembed(ctx: discord.Interaction, title: str, description: str=None, color: str=None, thumbnail: str=None, fields: str=None):
-    if color is not None: color = int(color, 16)
-
-    e = discord.Embed(title=title, color=color, description=description)
-    if thumbnail is not None: e.set_thumbnail(url=thumbnail)
+@tree.command(name="exec", description="Creates a message with an embed.", guild=guild)
+@app.check(is_dev)
+async def execute(ctx: discord.Interaction, code: str):
+    runner = compile(code, "user_input", "exec", flags=ast.PyCF_ALLOW_TOP_LEVEL_AWAIT)
+    coro: Awaitable | None = eval(runner)
+    if coro is not None: await coro
 
 bot.run(token)
